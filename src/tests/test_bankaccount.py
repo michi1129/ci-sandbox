e20f0f39d5b451e7419dc223c9af591c778ad12d
michi <ro.michi1129@gmail.com>
2018-05-17 20:14:06 +0900

    fail test

--- src/tests/test_bankaccount.py
+++ src/tests/test_bankaccount.py
@@ -5,4 +5,4 @@ from bankaccount import BankAccount
 class BankAccountTest(TestCase):
     def test_balance(self):
         acc = BankAccount(100)
-        self.assertEqual(100, acc.balance())
+        self.assertEqual(120, acc.balance())
